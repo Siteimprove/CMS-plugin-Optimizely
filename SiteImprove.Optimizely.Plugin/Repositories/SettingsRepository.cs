09460ad7bb2ee90bcba4767d7de9ec6accb86fcf
Ricardo Domingues <rdom@siteimprove.com>
2026-03-27 11:32:51 +0000

    PLIA-3882: Added fix for concurrency issues

--- SiteImprove.Optimizely.Plugin/Repositories/SettingsRepository.cs
+++ SiteImprove.Optimizely.Plugin/Repositories/SettingsRepository.cs
@@ -11,6 +11,8 @@ namespace SiteImprove.Optimizely.Plugin.Repositories
     [ServiceConfiguration(typeof(ISettingsRepository))]
     public class SettingsRepository : ISettingsRepository
     {
+        private static readonly object _lock = new object();
+
         private static DynamicDataStore SettingStore
         {
             get
@@ -21,51 +23,60 @@ namespace SiteImprove.Optimizely.Plugin.Repositories
 
         public string GetToken()
         {
-            var settings = SettingStore.LoadAll<Settings>().ToArray().FirstOrDefault();
-
-            if (settings == null || string.IsNullOrWhiteSpace(settings.Token))
+            lock (_lock)
             {
-                var siteimproveHelper = ServiceLocator.Current.GetInstance<ISiteimproveHelper>();
-                string token = siteimproveHelper.RequestToken();
-                SaveToken(token);
+                var settings = SettingStore.LoadAll<Settings>().ToArray().FirstOrDefault();
 
-                return token;
+                if (settings == null || string.IsNullOrWhiteSpace(settings.Token))
+                {
+                    var siteimproveHelper = ServiceLocator.Current.GetInstance<ISiteimproveHelper>();
+                    string token = siteimproveHelper.RequestToken();
+                    SaveToken(token);
+
+                    return token;
+                }
+
+                return settings.Token;
             }
-
-            return settings.Token;
         }
 
         public void SaveToken(string token, bool recheck = false, bool latestUI = true, string apiUser = null, string apiKey = null, Dictionary<string, string> urlMap = null)
         {
-            var current = SettingStore.LoadAll<Settings>().ToArray().FirstOrDefault();
-            if (current != null)
+            lock (_lock)
             {
-                current.Token = token;
-                current.Recheck = recheck;
-                current.LatestUI = latestUI;
-                current.ApiUser = apiUser;
-                current.ApiKey = apiKey;
-                current.UrlMap = urlMap;
-                SettingStore.Save(current, current.GetIdentity());
-                return;
-            }
+                var current = SettingStore.LoadAll<Settings>().ToArray().FirstOrDefault();
+                if (current != null)
+                {
+                    current.Token = token;
+                    current.Recheck = recheck;
+                    current.LatestUI = latestUI;
+                    current.ApiUser = apiUser;
+                    current.ApiKey = apiKey;
+                    current.UrlMap = urlMap;
+                    SettingStore.Save(current, current.GetIdentity());
+                    return;
+                }
 
-            SettingStore.Save(new Settings { Token = token, Recheck = recheck, LatestUI = latestUI, ApiUser = apiUser, ApiKey = apiKey });
+                SettingStore.Save(new Settings { Token = token, Recheck = recheck, LatestUI = latestUI, ApiUser = apiUser, ApiKey = apiKey });
+            }
         }
 
         public Settings GetSetting()
         {
-            var settings = SettingStore.LoadAll<Settings>().ToArray().FirstOrDefault();
-
-            if (settings == null || string.IsNullOrWhiteSpace(settings.Token))
+            lock (_lock)
             {
-                var siteimproveHelper = ServiceLocator.Current.GetInstance<ISiteimproveHelper>();
-                string token = siteimproveHelper.RequestToken();
-                SaveToken(token);
-                settings = SettingStore.LoadAll<Settings>().ToArray().Single(c => c.Token == token);
-            }
+                var settings = SettingStore.LoadAll<Settings>().ToArray().FirstOrDefault();
 
-            return settings;
+                if (settings == null || string.IsNullOrWhiteSpace(settings.Token))
+                {
+                    var siteimproveHelper = ServiceLocator.Current.GetInstance<ISiteimproveHelper>();
+                    string token = siteimproveHelper.RequestToken();
+                    SaveToken(token);
+                    settings = SettingStore.LoadAll<Settings>().ToArray().FirstOrDefault(c => c.Token == token);
+                }
+
+                return settings;
+            }
         }
     }
 }
